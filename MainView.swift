c1ae39747529df526a3d7fbb3c454e60597f4b0a
ColeL97 <129351240+ColeL97@users.noreply.github.com>
2023-03-31 01:34:21 -0400

    updated without 2 navbars

--- MainView.swift
+++ MainView.swift
@@ -5,30 +5,30 @@ struct MainView: View {
 
     var body: some View {
         TabView(selection: $selectedTab) {
-            MainTabView()
+            HomeView()
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
                 }.tag(0)
-            
+
             SearchView()
                 .tabItem {
                     Image(systemName: "magnifyingglass")
                     Text("Search")
                 }.tag(1)
-            
+
             CameraView()
                 .tabItem {
                     Image(systemName: "camera")
                     Text("Camera")
                 }.tag(2)
-            
+
             TeamView()
                 .tabItem {
                     Image(systemName: "person.3")
                     Text("Team")
                 }.tag(3)
-            
+
             ProfileView()
                 .tabItem {
                     Image(systemName: "person")
